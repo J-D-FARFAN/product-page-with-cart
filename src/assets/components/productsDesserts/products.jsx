8ebb0aa1dc01fb7599037d60e7782f4260b7b608
J-D-FARFAN <juan.frontendMJ@gmail.com>
2024-09-27 23:14:43 -0500

    upd product

--- src/assets/components/productsDesserts/products.jsx
+++ src/assets/components/productsDesserts/products.jsx
@@ -1,4 +1,5 @@
 import svgIconCart from "../../../../public/images/icon-add-to-cart.svg";
+import { useState } from "react";
 
 export function ProductsDesserts({
   typeProduct,
@@ -6,6 +7,14 @@ export function ProductsDesserts({
   priceProduct,
   imageProduct,
 }) {
+  const [btnState, setBtnState] = useState(false);
+
+  const handleClick = () => {
+    setBtnState(true);
+  };
+
+  const btnStateAdded = btnState ? "addCart addedProdutcCart" : "addCart";
+
   return (
     <>
       <article className="content-cardProductDessert">
@@ -14,10 +23,48 @@ export function ProductsDesserts({
             <img src={imageProduct} alt="" className="productDessert" />
           </div>
 
-          <button className="addCart">
-            <img src={svgIconCart} alt="" className="btn-addCart" />
-            Add to Cart
-          </button>
+          {!btnState ? (
+            <button className={btnStateAdded} onClick={handleClick}>
+              <img src={svgIconCart} alt="" className="btn-addCart" />
+              Add to Cart
+            </button>
+          ) : (
+            <button className={btnStateAdded} onClick={handleClick}>
+              <div className="content-iconAdded-product">
+                <svg
+                  className="iconAddedProduct"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10"
+                  height="2"
+                  fill="none"
+                  viewBox="0 0 10 2"
+                >
+                  <path
+                    className="iconAddedProduct-color"
+                    d="M0 .375h10v1.25H0V.375Z"
+                  />
+                </svg>
+              </div>
+
+              <span className="numberProduct">0</span>
+
+              <div className="content-iconAdded-product">
+                <svg
+                  className="iconAddedProduct"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10"
+                  height="10"
+                  fill="none"
+                  viewBox="0 0 10 10"
+                >
+                  <path
+                    className="iconAddedProduct-color"
+                    d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"
+                  />
+                </svg>
+              </div>
+            </button>
+          )}
         </figure>
 
         <section className="information-cardProductDessert">
